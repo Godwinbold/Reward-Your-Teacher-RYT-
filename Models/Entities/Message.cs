9a4db0fc4796dc7628b40e8d69ac460ed5ee4e7d
Godwinbold <gordonblaze5@gmail.com>
2024-01-30 08:25:46 +0100

    Reward Your Teacher(RYT)

---
+++ Models/Entities/Message.cs
@@ -0,0 +1,16 @@
+﻿namespace RYT.Models.Entities
+{
+    public class Message: BaseEntity
+    {
+        public string Text { get; set; } = string.Empty;
+        public string UserId { get; set; }= string.Empty;
+        public DateTime DeliverdOn { get; set; } = DateTime.UtcNow;
+        public string MessageId { get; set; } = string.Empty;
+        public string ReceiverId { get; set; } = string.Empty;
+        public DateTime ReadOn { get; set; } = DateTime.UtcNow;
+        public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+
+        // Navigation prop
+        public AppUser? User { get; set; }
+    }
+}
